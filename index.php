c3bf54f8f67691633ad9094c036bd7a9011f5a09
gokulchandrapr <gokulchandra048@gmail.com>
2018-03-19 07:54:18 +0000

    001

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 
 echo "<br>Welcome to the Area2";
+
+<html><head><title><?php echo "cool title";?></title></head>
+<body
+<?php 
+$image_url='https://www.google.com/intl/en_com/images/srpr/logo3w.png';
+?>
+
+<img src="<?php echo $image_url;?>">
+
+<br><h1>Got me?</h1>

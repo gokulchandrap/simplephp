8c36f5c381b1d7dd70054c23995a27915d75b18c
gokulchandrapr <gokulchandra048@gmail.com>
2018-03-19 08:04:22 +0000

    hill

--- index.php
+++ index.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang='en'>
-<head>
-    <title>Test Page</title>
-</head>
-<body>
- 
 <?php
-    echo "<img src='openshift_container_platform.png' alt='photo of me' />";
-?>
- 
-</body>
-</html>
 
+echo "<br>Welcome to the Area2";
+<img src="openshift_container_platform.png">

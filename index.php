8e47d76c07dcd42b1685638a584deb73b188fa60
gokulchandrapr <gokulchandra048@gmail.com>
2018-03-19 07:58:35 +0000

    hill

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <title>Test Page</title>
+</head>
+<body>
+ 
 <?php
-
-echo "<br>Welcome to the Area2";
-
-<html><head><title><?php echo "cool title";?></title></head>
-<body
-<?php 
-$image_url='https://www.google.com/intl/en_com/images/srpr/logo3w.png';
+    echo "<img src='openshift_container_platform.png' alt='photo of me' />";
 ?>
+ 
+</body>
+</html>
 
-<img src="<?php echo $image_url;?>">
-
-<br><h1>Got me?</h1>
